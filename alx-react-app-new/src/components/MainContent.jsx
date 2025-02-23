63cc2aad3849ef653637b0961736aebdc5fd7f8f
Melvin Freeman <thatguyzen99@gmail.com>
2025-02-23 21:49:19 +0100

    alx-react-app-new commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0', color: 'blue' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+};
+
+export default MainContent;
