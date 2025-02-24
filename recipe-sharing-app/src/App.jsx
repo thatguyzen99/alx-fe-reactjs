de938e84ccc7c4874eb7236cfa2d16f7be3b7ae3
Melvin Freeman <thatguyzen99@gmail.com>
2025-02-24 13:06:39 +0100

    recipe-sharing-app commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="./assets/react.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Recipe Sharing Application</h1>
+      <div className="card">
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+    </>
+  )
+}
+
+export default App;
