393c227d2cbd1223a386f623d8d2f91e3317b88e
Melvin Freeman <thatguyzen99@gmail.com>
2025-03-13 15:09:34 +0100

    Added Variants

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Replaces "purge"
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: { // Added for task compliance, though optional in v3+
     extend: {},
   },
   plugins: [],
