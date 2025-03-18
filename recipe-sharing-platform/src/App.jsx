41bea4e8487cd75f2e42e3651522bc9bad26a1bd
Melvin Freeman <thatguyzen99@gmail.com>
2025-03-18 09:03:00 +0100

    fix missing

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import RecipeDetail from "./pages/RecipeDetail";
-import AddRecipe from "./pages/AddRecipe";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold text-center">Recipe Sharing Platform</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/add" element={<AddRecipe />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 export default App;
